105fc8e80d379ae9d8a9a65d1d27217138c45034
ts-miller <tylerstephen11@gmail.com>
2021-01-11 15:26:25 -0500

    changed to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
-
-  post '/login', to: 'auth#login'
-  get '/auto_login', to: 'auth#auto_login'
-  get '/user_is_authed', to: 'auth#user_is_authed'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
