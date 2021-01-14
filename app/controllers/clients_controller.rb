3482cbe6c66be694f850af8b421d989f698aba04
ts-miller <tylerstephen11@gmail.com>
2021-01-13 21:21:16 -0500

    created clients model and controller as well as migrated db

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,36 @@
+class ClientsController < ApplicationController
+    before_action :set_client, only: [:update, :destroy]
+
+    def index
+        @clients = Client.all
+
+        render json: @clients
+    end
+
+    def create
+        @client = Client.new(client_params)
+
+        if @client.save
+            render json: @client, status: :created, location: @client
+        else
+            render json: @client.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+        def set_client
+            @client = Pet.find(params[:id])
+        end
+
+        def client_params
+            params.require(:pet).permit(:name, :age, :summary)
+        end
+
+end
