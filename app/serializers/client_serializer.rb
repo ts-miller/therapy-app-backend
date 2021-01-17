4e0cf66615710065cca6031504081bc1ad29a4d0
ts-miller <tylerstephen11@gmail.com>
2021-01-17 15:03:13 -0500

    updated readme with link to frontend

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,9 +1,4 @@
 class ClientSerializer < ActiveModel::Serializer
     attributes :id, :first_name, :last_name, :age, :location, :summary, :appointments
 
-    # def appointments
-    #     self.object.appointments.map do |app|
-    #         {}
-    #     end
-    # end
 end
